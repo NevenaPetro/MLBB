8f7ab506cd64cb10dc77e50941a21afb2e6a30c4
NevenaPetro <a.nevena.petrovic@gmail.com>
2023-04-17 16:43:24 +0200

    debugging login

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +12,12 @@ const firebaseConfig = {
   projectId: "mlbbapp",
   storageBucket: "mlbbapp.appspot.com",
   messagingSenderId: "535832969060",
-  appId: "1:535832969060:web:2e9e3ba0135b540c8a4471"
+  appId: "1:535832969060:web:2e9e3ba0135b540c8a4471",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+const auth = getAuth(app);
+const db = getFirestore();
+
+export { auth, db };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { db } from "../firebase.config";
 import { Link, useNavigate } from "react-router-dom";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
@@ -10,7 +12,7 @@ function LoginPage() {
     password: "",
   });
   const { email, password } = formData;
-
+  const navigate = useNavigate();
   const onChange = (event) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -18,9 +20,27 @@ function LoginPage() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredentials.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="email"
           placeholder="Email"
@@ -40,6 +60,7 @@ function LoginPage() {
           onClick={() => setShowPassword((prevState) => !prevState)}
         />
         <Link to="/forgot-password">Forgot Password?</Link>
+        <button>Log in</button>
       </form>
     </>
   );
